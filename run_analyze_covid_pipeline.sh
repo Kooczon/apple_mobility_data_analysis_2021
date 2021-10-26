98a4e293ea8ad1e43d23f7fb755b8d2ac4323eee
Keren Sheen Oczon <kooczon@dons.usfca.edu>
2021-10-25 17:20:16 -0700

    Add different state and data to bash

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -4,5 +4,5 @@
 # paramaters passed in from the shell
 
 Rscript -e "rmarkdown::render('Analysis.Rmd', \
-            params = list(state = 'Arizona', \
+            params = list(state = 'Colorado', \
             data = 'data/raw_data/applemobilitytrends-2021-09-18.csv'))"
