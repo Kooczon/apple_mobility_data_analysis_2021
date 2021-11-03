41e3a18f3b12daffe1c0e67366a7bd8af6d8acb5
Keren Sheen Oczon <kooczon.usfca.edu>
2021-11-03 02:11:00 +0000

    First commit of process sequence script

---
+++ code/process_sequences.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Keren Oczon
+# November 2, 2021
+# kooczon@dons.usfca.edu
+
+# Goal for this calculate and report the number of sequences in the file and tally the number of each
+# SARS-CoV-2 sequences in the dataset
+
