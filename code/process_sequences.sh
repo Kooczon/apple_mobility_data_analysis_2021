e0f8bb12e4cc2e12e0a864a5c5554439043790f4
Keren Sheen Oczon <kooczon.usfca.edu>
2021-11-15 21:21:58 +0000

    Modify the process sequences script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -4,6 +4,27 @@
 # November 2, 2021
 # kooczon@dons.usfca.edu
 
-# Goal for this calculate and report the number of sequences in the file and tally the number of each
-# SARS-CoV-2 sequences in the dataset
+# Bash script to process and analyze sequecnes in the compressed SARS-CoV-2 fasta file.
+# The goal is to gather and state the total number of sequences.
+# We also want to tally the sequences in the data from each country and
+# sort the output from largest to smallest
 
+# Having a quick check to state that the command line arguments are present
+if [ $# -eq 0 ]
+then
+        echo "Two arguments are needed to run the script:"
+        echo "First, provide the path to the fasta file sequence."
+        echo "Next, state 'ALL' if you would like a  detailed output."
+        exit 1
+fi
+
+
+# Tally SARS-CoV-2 sequences and sort by country
+# Check if total seq count is requested
+if [ "$2" = 'ALL' ]
+then
+    echo "The total number of sequences is:" "$(bioawk -c fastx 'END{print NR}' "$1" )"
+fi
+
+# Output sequences by country tally
+zgrep "isolate.*Homo" "$1" | cut -d"|" -f5 | sort | uniq -c | sort -rn
