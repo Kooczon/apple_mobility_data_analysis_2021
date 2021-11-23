0a48ecefe5164dad0b34736509ca613f8a7bbf55
Keren Sheen Oczon <kooczon.usfca.edu>
2021-11-23 23:26:53 +0000

    Modify run pipeline to take third variable

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# A bas script to drive the rendering of an Rmarkdown file using some
+set -eou pipefail
+
+# A bash script to drive the rendering of an Rmarkdown file using some
 # paramaters passed in from the shell
 # this script expects two command line parameters
 # the fisrt should be a US state, and the second is the raw apple mobility
@@ -12,18 +14,17 @@
 
 if [ $# -eq 0 ]
 then
-  echo "To run this script, supply two arguments:"
+  echo "To run this script, supply three arguments:"
   echo "The first is the name of the state to be analyzed."
-  echo "The second is the path too the mobility data csv file."
+  echo "The second is the path two the mobility data csv file."
+  echo "The third is the path to the sequence summary report text file"
   exit 1
 fi
 
 state=$1
 state=${state// /_}
 
+RMD_PARAMS="params = list(state = '$1', data = '$2'), seqdata = '$3')"
+RMD_OUTPUT="output_dir = 'output', output_file = 'Analysis_$state'"
 
-Rscript -e "rmarkdown::render('Analysis.Rmd',\
-            params = list(state = '$1',\
-            data = '$2'),\
-            output_dir = 'output',\
-            output_file = 'Analysis_$state')"
+Rscript -e "rmarkdown::render('Analysis.Rmd', $RMD_PARAMS, $RMD_OUTPUT)"
