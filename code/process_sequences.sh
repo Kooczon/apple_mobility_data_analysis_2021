1695e4f60aea7d300ef6f60078145bf938780757
Keren Sheen Oczon <kooczon.usfca.edu>
2021-11-23 03:10:15 +0000

    Finish the script

--- code/process_sequences.sh
+++ code/process_sequences.sh
@@ -4,7 +4,7 @@
 # November 2, 2021
 # kooczon@dons.usfca.edu
 
-# Bash script to process and analyze sequecnes in the compressed SARS-CoV-2 fasta file.
+# Bash script to process and analyze sequences in the compressed SARS-CoV-2 fasta file.
 # The goal is to gather and state the total number of sequences.
 # We also want to tally the sequences in the data from each country and
 # sort the output from largest to smallest
@@ -12,19 +12,22 @@
 # Having a quick check to state that the command line arguments are present
 if [ $# -eq 0 ]
 then
-        echo "Two arguments are needed to run the script:"
+        echo "One argument is needed to run the script:"
         echo "First, provide the path to the fasta file sequence."
-        echo "Next, state 'ALL' if you would like a  detailed output."
         exit 1
 fi
 
+# Check if the file has something in it 
+if ! grep -q '[^[:space:]]' "$1"; then
+	echo "There is nothing in the file."
+else
+	echo "There is data in the file"
+fi
 
 # Tally SARS-CoV-2 sequences and sort by country
 # Check if total seq count is requested
-if [ "$2" = 'ALL' ]
-then
-    echo "The total number of sequences is:" "$(bioawk -c fastx 'END{print NR}' "$1" )"
-fi
+	echo "The total number of sequences is:"
+	bioawk -c fastx 'END{print NR}' "$1"
 
 # Output sequences by country tally
-zgrep "isolate.*Homo" "$1" | cut -d"|" -f5 | sort | uniq -c | sort -rn
+bioawk -c fastx '{print $comment}' "$1" | awk '{split($0,x,"|");print x[21]}' | sort | uniq -c | sort -rn
